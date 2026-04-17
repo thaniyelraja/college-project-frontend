9c2000cb69a6baae73c9a858754efaefe8caace4
thaniyelraja <rajaselvan2003@gmail.com>
2026-04-17 10:35:19 +0530

    fix: auth config

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -15,6 +15,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: 'select_account' });
 export const yahooProvider = new OAuthProvider('yahoo.com');
 
 export { signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, deleteUser, getAdditionalUserInfo, sendEmailVerification };
